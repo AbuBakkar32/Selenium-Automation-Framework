3e63fe9953eb701c572af814520e78e3300a6383
Abubakkar32 <abubakkar.swe@gmail.com>
2022-04-27 16:07:00 +0600

    First Commit 27-04-2022

--- test/storeFrontAnalysis.js
+++ test/storeFrontAnalysis.js
@@ -1,8 +1,8 @@
-const {By, Key, Builder, Browser} = require("selenium-webdriver");
+const {By, Key, Builder, Browser, until} = require("selenium-webdriver");
 require("chromedriver");
 var should = require('chai').should()
 
-describe("Getting Store Front Menu......", function () {
+describe("Getting Store Front Analysis Menu......", function () {
     let driver;
     beforeEach(async function () {
         driver = await new Builder().forBrowser("chrome").build();
@@ -10,7 +10,7 @@ describe("Getting Store Front Menu......", function () {
     afterEach(async function () {
         await driver.quit();
     });
-    it("Getting Store Front", async function () {
+    it("Getting Store Front Analysis", async function () {
 
         //To fetch http://google.com from the browser with our code.
         await driver.get("https://www.cart.com/");
@@ -19,9 +19,10 @@ describe("Getting Store Front Menu......", function () {
         var elem = await driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/div[1]"));
         await actions.move({duration: 2000, origin: elem, x: 0, y: 0}).perform();
         await driver.sleep(1000);
-        await driver.findElement(By.linkText("Storefront Analytics")).click();
-        await driver.findElement(By.className("offering--hero__heading")).getText().then(function (text) {
-            if(text=="How powerful is your online store?"){
+        await driver.findElement(By.xpath("//*[@id=\"w-dropdown-list-0\"]/a[2]")).click();
+        await driver.wait(until.elementLocated(By.xpath("/html/body/main/section[2]/div/div/h2")), 10000);
+        await driver.findElement(By.xpath("/html/body/main/section[2]/div/div/h2")).getText().then(function (text) {
+            if(text=="Holistic ecommerce storefront analytics"){
                 driver.quit();
             }
             else{
